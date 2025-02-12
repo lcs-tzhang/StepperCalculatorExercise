3e953364552ffe68e0358d37b82cabe6c9d10261
Tiffany Zhang <tzhang@lcs.on.ca>
2025-02-12 09:12:18 -0500

    Initial Commit

---
+++ StepperCalculatorExercise/StepperCalculatorExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperCalculatorExerciseApp.swift
+//  StepperCalculatorExercise
+//
+//  Created by xinyu zhang on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct StepperCalculatorExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
