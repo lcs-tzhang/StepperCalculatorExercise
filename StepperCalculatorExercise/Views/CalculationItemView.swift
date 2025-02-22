08a7b108be6bd0a0760605f9024e47c1c3a60319
Tiffany Zhang <tzhang@lcs.on.ca>
2025-02-22 13:30:39 -0500

    added a basic itemview

---
+++ StepperCalculatorExercise/Views/CalculationItemView.swift
@@ -0,0 +1,15 @@
+//
+//  CalculationItemView.swift
+//  StepperCalculatorExercise
+//
+//  Created by xinyu zhang on 2025-02-22.
+//
+
+import Foundation
+
+struct CalculationItemView : View{
+    let result: CalculationResult
+    var body: some View {
+        
+    }
+}
